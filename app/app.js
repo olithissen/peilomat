0ee6922f4bcc7135ca96ff55486b3857ec748fde
Oliver Thissen <oliver.thissen@generali.com>
2018-10-01 14:58:06 +0200

    Adds heading and heading sector

--- app/app.js
+++ app/app.js
@@ -5,10 +5,16 @@ var measure = {
         target: null
     },
 
+    getHeading: function() {
+        return (360 + this.getBearing()) % 360;
+    },
+
+    getBearing: function() {
+        return turf.bearing(this.leafletToTurf(this.markers.start.getLatLng()), this.leafletToTurf(this.markers.lookAt.getLatLng()));
+    },
+
     updateTarget: function () {
-        var bearing = turf.bearing(this.leafletToTurf(this.markers.start.getLatLng()), this.leafletToTurf(this.markers.lookAt.getLatLng()));
-        console.log(bearing);
-        var destination = turf.destination(this.leafletToTurf(this.markers.start.getLatLng()), 50, bearing, "kilometers");
+        var destination = turf.destination(this.leafletToTurf(this.markers.start.getLatLng()), 50, this.getBearing(), "kilometers");
         this.markers.target.setLatLng([destination.geometry.coordinates[1], destination.geometry.coordinates[0]]);
     },
 
@@ -30,6 +36,10 @@ function update(sender) {
         measure.updateTarget();
         firstpolyline.setLatLngs(measure.getLookAtVector());
         secondpolyline.setLatLngs(measure.getTargetVector());
+        measure.markers.start.setTooltipContent(Math.round(measure.getHeading()).toString() + "°");
+        sector.setAngles(0, measure.getHeading());
+        sector.setLatLng(measure.markers.start.getLatLng());
+
     }
 }
 
@@ -45,6 +55,8 @@ measure.markers.start = L.marker([51.0053536788148, 5.9861111640930185], {
     "draggable": true,
     "title": "Start"
 }).on("drag", update("start")).addTo(map);
+measure.markers.start.bindTooltip("My Label", {permanent: true, className: "my-label", offset: [0, 0] });
+
 
 measure.markers.lookAt = L.marker([51.0032506646835, 5.98658323287964], {
     "draggable": true,
@@ -54,7 +66,7 @@ measure.markers.lookAt = L.marker([51.0032506646835, 5.98658323287964], {
 measure.markers.target = L.marker([52, 6], {
     "title": "Target"
 }).addTo(map);
-measure.updateTarget();
+//measure.updateTarget();
 
 var firstpolyline = L.polyline(measure.getLookAtVector(), {
     color: 'red',
@@ -72,6 +84,7 @@ var secondpolyline = L.polyline(measure.getTargetVector(), {
 });
 secondpolyline.addTo(map);
 
-const newLocal = measure.markers.start.getLatLng();
-var bearingIndicator = L.circle(newLocal, {radius: "5000"});
-bearingIndicator.addTo(map);
+var sector = L.circle(measure.markers.start.getLatLng(), {
+	radius: 5000
+});
+sector.addTo(map);
