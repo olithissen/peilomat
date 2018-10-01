a6d78a53dc7cd7e4b3e77aa2022c839fb3659f3b
Oliver Thissen <oliver.thissen@generali.com>
2018-10-01 12:55:52 +0200

    Started working on bearing marker

---
+++ app/app.js
@@ -0,0 +1,77 @@
+var measure = {
+    markers: {
+        start: null,
+        lookAt: null,
+        target: null
+    },
+
+    updateTarget: function () {
+        var bearing = turf.bearing(this.leafletToTurf(this.markers.start.getLatLng()), this.leafletToTurf(this.markers.lookAt.getLatLng()));
+        console.log(bearing);
+        var destination = turf.destination(this.leafletToTurf(this.markers.start.getLatLng()), 50, bearing, "kilometers");
+        this.markers.target.setLatLng([destination.geometry.coordinates[1], destination.geometry.coordinates[0]]);
+    },
+
+    getLookAtVector: function () {
+        return [this.markers.start.getLatLng(), this.markers.lookAt.getLatLng()];
+    },
+
+    getTargetVector: function () {
+        return [this.markers.lookAt.getLatLng(), this.markers.target.getLatLng()];
+    },
+
+    leafletToTurf: function (latLng) {
+        return [latLng.lng, latLng.lat];
+    }
+};
+
+function update(sender) {
+    return function (e) {
+        measure.updateTarget();
+        firstpolyline.setLatLngs(measure.getLookAtVector());
+        secondpolyline.setLatLngs(measure.getTargetVector());
+    }
+}
+
+var map = L.map('map').setView([51, 6], 10);
+mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' + mapLink + ' Contributors',
+        maxZoom: 18
+    }).addTo(map);
+
+measure.markers.start = L.marker([51.0053536788148, 5.9861111640930185], {
+    "draggable": true,
+    "title": "Start"
+}).on("drag", update("start")).addTo(map);
+
+measure.markers.lookAt = L.marker([51.0032506646835, 5.98658323287964], {
+    "draggable": true,
+    "title": "LookAt"
+}).on("drag", update("lookAt")).addTo(map);
+
+measure.markers.target = L.marker([52, 6], {
+    "title": "Target"
+}).addTo(map);
+measure.updateTarget();
+
+var firstpolyline = L.polyline(measure.getLookAtVector(), {
+    color: 'red',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+});
+firstpolyline.addTo(map);
+
+var secondpolyline = L.polyline(measure.getTargetVector(), {
+    color: 'blue',
+    weight: 3,
+    opacity: 0.5,
+    smoothFactor: 1
+});
+secondpolyline.addTo(map);
+
+const newLocal = measure.markers.start.getLatLng();
+var bearingIndicator = L.circle(newLocal, {radius: "5000"});
+bearingIndicator.addTo(map);
